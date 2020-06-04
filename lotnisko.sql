cacceb967f3093fb969fded857e68b06132d6b7c
Duk001 <46705619+Duk001@users.noreply.github.com>
2020-06-04 18:38:03 +0200

    Add files via upload

--- lotnisko.sql
+++ lotnisko.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 01 Cze 2020, 22:54
+-- Czas generowania: 04 Cze 2020, 18:23
 -- Wersja serwera: 10.4.11-MariaDB
 -- Wersja PHP: 7.4.6
 
@@ -37,6 +37,17 @@ CREATE TABLE `lot` (
   `Destynacja` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `lot`
+--
+
+INSERT INTO `lot` (`Nazwa_lotu`, `Numer_rejestracyjny`, `Model_samolotu`, `Godzina_odlotu`, `Godzina_przylotu`, `Bramka`, `Destynacja`) VALUES
+('AF7777', 'L190', 'airbus a380', '19/6/18 13:55:26', '19/6/18 15:55:26', 'A21', 'Madryt'),
+('LF0021', 'K212', 'boeing 777', '25/6/20 03:00:00', '25/6/20 22:40:00', 'A10', 'Kapsztad'),
+('RY1082', 'L191', 'airbus a380', '25/6/20 11:03:26', '25/6/20 15:40:26', 'A22', 'Londyn'),
+('RY1112', 'L293', 'Airbus A340', '25/6/20 20:00:00', '25/6/20 23:40:00', 'A22', 'Londyn'),
+('RY2135', 'L294', 'Airbus A380', '25/6/20 07:00:00', '25/6/20 18:40:00', 'A10', 'Nowy Jork');
+
 -- --------------------------------------------------------
 
 --
@@ -52,6 +63,17 @@ CREATE TABLE `pasazer` (
   `Status_odprawy` varchar(20) NOT NULL,
   `Numer_fotela` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `pasazer`
+--
+
+INSERT INTO `pasazer` (`Numer_biletu`, `Imie`, `Nazwisko`, `Nazwa_lotu`, `Typ_bagazu`, `Status_odprawy`, `Numer_fotela`) VALUES
+(34, 'Ewa', 'Kowalska', 'LF0021', ' checked baggage', 'Checked in', 46),
+(100, 'Maciej', 'Doktor', 'AF7777', 'None', 'Checked in', 23),
+(245, 'Jan', 'Duda', 'RY1112', 'None', 'Checked in', 74),
+(342, 'Anna', 'Komnena', 'RY1082', 'None', 'Checked in', 84),
+(921, 'Adam', 'Nowak', 'RY1112', 'None', 'Checked in', 21);
 
 --
 -- Indeksy dla zrzutów tabel
